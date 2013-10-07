77991de3a1c37504ac71b2760f957a2aa88b67b1
Kevin Montag <kmontag@cs.stanford.edu>
2013-10-06 20:13:17 -0700

    Initial event listeners and server setup

---
+++ bin/mpdws.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var argv = require('optimist')
+    .usage("Create a WebSockets-compatible server to read and control an MPD server\nUsage: $0")
+    .describe('http-port', 'Port for incoming connections')
+    .describe('http-host', 'Hostname for incoming connections (allows all hostnames by default)')
+    .describe('mpd-host', 'Hostname for the outgoing MPD connection')
+    .describe('mpd-port', 'Port for the outgoing MPD connection')
+    .describe('help', 'Show this message and exit')
+    .boolean('help')
+    .default({
+        'http-port': 8080,
+        'mpd-host': '127.0.0.1',
+        'mpd-port': 6600,
+    })
+    .argv;
+
+if (argv.help) {
+    console.log(require('optimist').help());
+    process.exit(0);
+}
+
+var mpdws = require('../lib/mpdws.js');
+var http = require('http');
+var httpServer = http.createServer().listen(argv['http-port'], argv['http-host'], function() {
+    console.log('HTTP Server listening on port ' + argv['http-port'])
+});
+var server = mpdws.createServer(httpServer).connect({
+    port: argv['mpd-port'],
+    host: argv['mpd-host'],
+}, function() {
+    console.log('Connected to MPD server at ' + argv['mpd-host'] + ':' + argv['mpd-port']);
+});
